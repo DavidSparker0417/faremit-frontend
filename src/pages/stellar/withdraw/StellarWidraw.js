4cac30ff6ec3561c912f42c03c538be4fc75077f
DavidSparker <davidsparker0417@gmain.com>
2024-04-23 01:43:27 +0900

    testing ::

--- src/pages/stellar/withdraw/StellarWidraw.js
+++ src/pages/stellar/withdraw/StellarWidraw.js
@@ -140,10 +140,9 @@ const StellarWithdraw = () => {
                         fee: charge,
                         amount: withdraw.amount,
                         wallet_address: wallet,
-                        bank_name: selected.bank_name,
-                        account_number: accountNumber,
+                        // bank_name: selected.bank_name,
+                        // account_number: accountNumber,
                         // account_name: fullName [DAVID]
-                        account_name: null
                         // Hex: data.HexValue,
                     }
                 }
